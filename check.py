8730578cc515f5193aa1b1118e77f68bf5bed17d
Mohammad Mustak Absar Khan <mustak.absar.khan@gmail.com>
2023-04-29 20:14:35 +0600

    nvidia_driver_updator.exe file added & python code updated for error catch and bug fix

--- check.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-output = subprocess.check_output(['nvidia-smi', '--query-gpu=driver_version', '--format=csv'])
-driver_version = output.decode().split('\n')[1]
-
-print(f"The current NVIDIA driver version is: {driver_version}")
